fc1a5cbbe1db18af8786c5b0a7c55aab269e7d45
Hashim <hashimmohammed212@gmail.com>
2025-11-14 11:49:09 +0530

    feat:converted proxy and mockdata to TS

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function proxy(request) {
+export function proxy(request: NextRequest) {
   return NextResponse.next();
 }
 
